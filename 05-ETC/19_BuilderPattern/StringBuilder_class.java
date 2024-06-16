34b42d67d2c127d742f7174dfcb617a0cadbc5f2
wlstjd <xkakrlfh@naver.com>
2024-06-16 20:06:50 +0900

    builder pattern

---
+++ 05-ETC/19_BuilderPattern/StringBuilder_class.java
@@ -0,0 +1,26 @@
+class StudentBuilder {
+    private int id;
+    private String name;
+    private String grade;
+    private String phoneNumber;
+
+    public StudentBuilder id(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public StudentBuilder name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public StudentBuilder grade(String grade) {
+        this.grade = grade;
+        return this;
+    }
+
+    public StudentBuilder phoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        return this;
+    }
+}
